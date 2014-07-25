d29a257f0751300fd931a72529ebb524b98f8499
Sam Blackman <sblackman@gmail.com>
2014-07-25 18:29:50 -0400

    add state and basic behaviour to card model

--- card.rb
+++ card.rb
@@ -1,12 +1,19 @@
 class Card
+  attr_reader :front, :back, :id
+
   def initialize(args)
-    @id = id
-    @front = front
-    @back = back
+    @id = args[:id]
+    @front = args[:front]
+    @back = args[:back]
   end
 
-  def correct?(side_guessed, answer)
-    side = instance_variable_get("@#{side_guessed}")
-    side == answer
+  # Future feature for reversing card guessing
+  # def correct?(side_guessed, answer)
+  #   side = self.send(side_guessed)
+  #   side == answer
+  # end
+
+  def correct?(user_answer)
+    user_answer.downcase == back.downcase
   end
 end
