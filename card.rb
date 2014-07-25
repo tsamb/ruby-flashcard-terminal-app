77c4b24d7c83d04db017b7ec6376bda9d9fab848
Sam Blackman <sblackman@gmail.com>
2014-07-25 14:47:36 -0400

    add correct method to Card

--- card.rb
+++ card.rb
@@ -1,7 +1,12 @@
 class Card
   def initialize(args)
-
+    @id = id
+    @front = front
+    @back = back
   end
 
-
+  def correct?(side_guessed, answer)
+    side = instance_variable_get("@#{side_guessed}")
+    side == answer
+  end
 end
